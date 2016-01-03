e08192de665e29991012ca78bdb6b80b24aca869
kristjan <kpeterso@itcollege.ee>
2016-01-03 20:29:38 +0200

    advancement in Controller.java

--- src/Registraator/Controller.java
+++ src/Registraator/Controller.java
@@ -1,7 +1,68 @@
 package Registraator;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
 /**
  * Created by kristjan on 13/12/15.
  */
 public class Controller {
+    Stage stage = new Stage();
+    BorderPane mainPane;
+    GridPane midPane;
+    VBox leftPane;
+    Button viewVisitors;
+    Button checkInVisitor;
+    Button checkOutVisitor;
+    Button viewHosts;
+    Button addHosts;
+
+    int rows = 3;
+    int columns = 4;
+
+
+    public Controller() {
+        setupScene();
+        stage.show();
+    }
+
+    private void setupScene() {
+        mainPane = new BorderPane();
+        midPane = new GridPane();
+        leftPane = new VBox();
+        setupMidPane();
+        setupLeftPane();
+        mainPane.setCenter(midPane);
+        Scene scene = new Scene(mainPane);
+        stage.setScene(scene);
+        stage.setOnCloseRequest(event -> System.exit(0));
+
+    }
+
+    private void setupLeftPane() {
+
+
+    }
+
+    private void setupMidPane() {
+        for (int i = 0; i <rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                Label info = new Label();
+                info.setMinWidth(150);
+                info.setMaxWidth(150);
+                midPane.add(info, j, i);
+            }
+
+        }
+        midPane.setGridLinesVisible(true);
+
+    }
+
 }
--- src/Registraator/Main.java
+++ src/Registraator/Main.java
@@ -1,11 +1,16 @@
 package Registraator;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Created by kristjan on 13/12/15.
  */
-public class Main {
-    public static void main(String[] args) {
-        Database db = new Database();
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        new Controller();
+        //Database db = new Database();
 
     }
 }
